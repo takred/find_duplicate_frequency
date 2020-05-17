9fb1a8e1806560a157444cb475cbd29fb0ad8a54
takred <sazonov.kos@yandex.ru>
2020-05-17 16:00:14 +0300

    Рассчёт о повторах Person'ов и удаление дубликатов из списка вынесены в отдельные методы.

--- src/main/java/takred/Main.java
+++ src/main/java/takred/Main.java
@@ -5,6 +5,41 @@ import java.util.List;
 
 public class Main {
 
+    public static List<DuplicateFrequency> duplicateFrequenciesPerson(List<Person> personList) {
+        List<DuplicateFrequency> duplicateFrequencyList = new ArrayList<>();
+
+        for (int i = 0; i < personList.size(); i++) {
+            duplicateFrequencyList.add(new DuplicateFrequency(personList.get(i), 1));
+            for (int j = 0; j < personList.size(); j++) {
+                if (j != i) {
+                    DuplicateFrequency duplicateFrequency = duplicateFrequencyList.get(i);
+                    if (personList.get(i).equals(personList.get(j))) {
+                        duplicateFrequencyList.set(i, duplicateFrequency.setFrequency(duplicateFrequency.getFrequency() + 1));
+                    }
+                }
+            }
+        }
+        return duplicateFrequencyList;
+    }
+
+    public static List<DuplicateFrequency> deleteDuplicate(List<DuplicateFrequency> duplicateFrequencyList) {
+        List<DuplicateFrequency> sortDuplicateFrequencyList = new ArrayList<>();
+        sortDuplicateFrequencyList.add(duplicateFrequencyList.get(0));
+        for (int i = 0; i < duplicateFrequencyList.size(); i++) {
+            for (int j = 0; j < sortDuplicateFrequencyList.size(); j++) {
+                DuplicateFrequency duplicateFrequency = duplicateFrequencyList.get(i);
+                DuplicateFrequency sortDuplicateFrequency = sortDuplicateFrequencyList.get(j);
+                if (duplicateFrequency.getPerson().equals(sortDuplicateFrequency.getPerson())) {
+                    break;
+                }
+                if (j == sortDuplicateFrequencyList.size() - 1) {
+                    sortDuplicateFrequencyList.add(duplicateFrequencyList.get(i));
+                }
+            }
+        }
+        return sortDuplicateFrequencyList;
+    }
+
     public static void main(String[] args) {
         List<Person> personList = new ArrayList<>();
         personList.add(new Person("Иван", "Иванов", 1234567));
@@ -20,33 +55,9 @@ public class Main {
         personList.add(new Person("Валентин", "Валентинов", 1236567));
         personList.add(new Person("Евлампий", "Евлампов", 9234567));
 
-        List<DuplicateFrequency> duplicateFrequencyList = new ArrayList<>();
-        for (int i = 0; i < personList.size(); i++) {
-            duplicateFrequencyList.add(new DuplicateFrequency(personList.get(i), 1));
-            for (int j = 0; j < personList.size(); j++) {
-                if (j != i) {
-                    DuplicateFrequency duplicateFrequency = duplicateFrequencyList.get(i);
-                    if (personList.get(i).equals(personList.get(j))) {
-                        duplicateFrequencyList.set(i, duplicateFrequency.setFrequency(duplicateFrequency.getFrequency() + 1));
-                    }
-                }
-            }
-        }
+        List<DuplicateFrequency> duplicateFrequencyList = new ArrayList<>(duplicateFrequenciesPerson(personList));
 
-        List<DuplicateFrequency> sortDuplicateFrequencyList = new ArrayList<>();
-        sortDuplicateFrequencyList.add(duplicateFrequencyList.get(0));
-        for (int i = 0; i < duplicateFrequencyList.size(); i++) {
-            for (int j = 0; j < sortDuplicateFrequencyList.size(); j++) {
-                DuplicateFrequency duplicateFrequency = duplicateFrequencyList.get(i);
-                DuplicateFrequency sortDuplicateFrequency = sortDuplicateFrequencyList.get(j);
-                if (duplicateFrequency.getPerson().equals(sortDuplicateFrequency.getPerson())) {
-                    break;
-                }
-                if (j == sortDuplicateFrequencyList.size() - 1) {
-                    sortDuplicateFrequencyList.add(duplicateFrequencyList.get(i));
-                }
-            }
-        }
+        List<DuplicateFrequency> sortDuplicateFrequencyList = new ArrayList<>(deleteDuplicate(duplicateFrequencyList));
 
         for (int i = 0; i < sortDuplicateFrequencyList.size(); i++) {
             DuplicateFrequency sortDuplicateFrequency = sortDuplicateFrequencyList.get(i);
